4a95cf77e922dc5d3f03b6f10d4e4d54439e0cc1
zwzwzwzwa <137084696+zwzwzwzwa@users.noreply.github.com>
2023-11-09 21:34:00 +0800

    Update example-custom-script-2.sh

--- example-custom-script-2.sh
+++ example-custom-script-2.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 # Copyright (c) 2022-2023 Curious <https://www.curious.host>
 #
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
+# 本软件为自由软件，采用 MIT 许可授权。
+# 更多信息请参见 /LICENSE。
 # 
 # https://github.com/Curious-r/OpenWrtBuildWorkflows
-# Description: Automatically check OpenWrt source code update and build it. No additional keys are required.
+# 描述： 自动检查 OpenWrt 源代码更新并构建它。无需额外密钥。
 #-------------------------------------------------------------------------------------------------------
 #
 #
-# Patching is generally recommended.
-# # Here's a template for patching:
+# 一般建议打补丁。
+# 下面是一个补丁模板：
 #touch example.patch
 #cat>example.patch<<EOF
-#patch content
+# 补丁内容
 #EOF
 #git apply example.patch
 
