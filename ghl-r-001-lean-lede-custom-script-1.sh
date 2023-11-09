e69d52f4026da387133b58240c7ffb7c93978c96
zwzwzwzwa <137084696+zwzwzwzwa@users.noreply.github.com>
2023-11-09 21:36:31 +0800

    Update ghl-r-001-lean-lede-custom-script-1.sh

--- ghl-r-001-lean-lede-custom-script-1.sh
+++ ghl-r-001-lean-lede-custom-script-1.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 # Copyright (c) 2022-2023 Curious <https://www.curious.host>
 #
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
+# 本软件为自由软件，采用 MIT 许可授权。
+# 更多信息请参见 /LICENSE。
 # 
 # https://github.com/Curious-r/OpenWrtBuildWorkflows
-# Description: Automatically check OpenWrt source code update and build it. No additional keys are required.
+# 描述： 自动检查 OpenWrt 源代码更新并构建它。无需额外密钥。
 #-------------------------------------------------------------------------------------------------------
 #
 #
-# This script will run before feeds update, something you want to do at that moment should be written here.
-# A common function of this script is to modify the cloned OpenWrt source code. 
+# 该脚本将在源代码更新前运行，此时您要做的事情应写在这里。
+# 该脚本的一个常见功能是修改克隆的 OpenWrt 源代码。
 #
-# For instance, you can edit the feeds.conf.default to induct packages you need.
-# This is followed by some editing examples.
-# # Clear the feeds.conf.default and append the feed sources you need one by one:
+# 例如，您可以编辑 feeds.conf.default，以导入您需要的软件包。
+# 接下来是一些编辑示例。
+# # 清除 feeds.conf.default，然后逐一添加所需的 feed 源：
 #cat /dev/null > a.txt
 #echo 'src-git packages https://git.openwrt.org/feed/packages.git;openwrt-22.03' >> feeds.conf.default
 #echo 'src-git luci https://git.openwrt.org/project/luci.git;openwrt-22.03' >> feeds.conf.default
 #echo 'src-git routing https://git.openwrt.org/feed/routing.git;openwrt-22.03' >> feeds.conf.default
 #echo 'src-git telephony https://git.openwrt.org/feed/telephony.git;openwrt-22.03' >> feeds.conf.default
-# # Replace a feed source with what you want:
+# # 将 feed 源替换为您想要的内容：
 #sed '/feeds-name/'d feeds.conf.default
 #echo 'method feed-name path/URL' >> feeds.conf.default
-# # Uncomment a feed source:
-#sed -i 's/^#\(.*feed-name\)/\1/' feeds.conf.default
-# # Replace src-git with src-git to reduce the depth of cloning:
+# # 取消对馈源的注释：
+#sed -i 's/^#\(.* feed-name\)/\1/' feeds.conf.default
+# 用 src-git 替换 src-git，以减少克隆深度：
 #sed -i 's/src-git/src-git/g' feeds.conf.default
 #
-# You can also modify the source code by patching.
-# # Here's a template for patching:
+# 你也可以通过打补丁的方式修改源代码。
+# 下面是打补丁的模板：
 #touch example.patch
 #cat>example.patch<<EOF
-#patch content
+# 补丁内容
 #EOF
 #git apply example.patch
 
