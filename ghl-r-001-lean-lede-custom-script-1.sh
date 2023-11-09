97edc682570d5faf1d0d3a84b225b1e5fd7e1630
starlight <137084696+zwzwzwzwa@users.noreply.github.com>
2023-11-10 00:11:31 +0800

    Update ghl-r-001-lean-lede-custom-script-1.sh

--- ghl-r-001-lean-lede-custom-script-1.sh
+++ ghl-r-001-lean-lede-custom-script-1.sh
@@ -39,4 +39,5 @@
 
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small.git # passwall 基本依赖' >> feeds.conf.default
+echo 'src-git https://github.com/pyrovski/wrtbwmon.git' >> feeds.conf.default
 git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go/
