5cc1066559fa44e6d7aa2414e01988ba20716735
starlight <137084696+zwzwzwzwa@users.noreply.github.com>
2023-11-11 00:46:19 +0800

    Update ghl-r-001-openwrt-custom-script-1.sh

--- ghl-r-001-openwrt-custom-script-1.sh
+++ ghl-r-001-openwrt-custom-script-1.sh
@@ -37,7 +37,7 @@
 #git apply example.patch
 
 
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >> feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small.git # passwall 基本依赖' >> feeds.conf.default
-echo 'src-git wrtbwmon https://github.com/pyrovski/wrtbwmon.git' >> feeds.conf.default 
+echo 'src-git wrtbwmon https://github.com/pyrovski/wrtbwmon' >> feeds.conf.default 
 git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go/
